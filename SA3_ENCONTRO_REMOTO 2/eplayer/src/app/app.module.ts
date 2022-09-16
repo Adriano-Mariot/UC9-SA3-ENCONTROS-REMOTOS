76448ccdf590721d5ae7377d5a0e947ae7c5e8c7
Adrian Mariot <adrianomariot@hotmail.com>
2022-09-16 20:46:21 -0300

    enviando encontro remoto 2

---
+++ SA3_ENCONTRO_REMOTO 2/eplayer/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProdutoComponent } from './views/produto/produto.component';
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProdutoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ SA3_ENCONTRO_REMOTO 2/eplayer/src/app/views/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  //  nossas variáveis e métodos
+  userModel = new User()
+
+  receberDados() {
+    console.log(this.userModel);
+    alert("Dados enviados!")
+    
+  }
+
+  teste() {
+    this.userModel.email = "Iniciar registro"
+  }
+
+}
